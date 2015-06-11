b3561ea2ce59274ed7e6049257c8681c866345b3
mohsin <mohsin.rafi@kwanso.com>
2015-06-11 19:10:51 +0500

    admin authentication added for post

--- server/lib/config/seeds.js
+++ server/lib/config/seeds.js
@@ -11,8 +11,35 @@ if (Meteor.isServer) {
     return Meteor.roles.find({});
   });
 
+  if (Meteor.users.find().count() === 0) {
+    var admins = [
+      { name: "Admin", email: "test@kwanso.com", password: "admin", phone: '12345678', roles: ['admin'] }
+    ];
+    Accounts.emailTemplates.verifyEmail.text = function(user, url) {
+      return "Dear " + user.profile.name + ",\n\n" +
+        'To verify your account email, simply click the link below. ' + url +
+        "\n\n" + "Thanks";
+    };
+    _.each(admins, function (user) {
+      var id;
 
-var adminUser = Meteor.users.findOne({roles:{$in:["admin"]}});
+      id = Accounts.createUser({
+        email: user.email,
+        password: user.password,
+        profile: { name: user.name, phone: user.phone }
+      });
+
+      if (user.roles.length > 0) {
+        // Need _id of existing user record so this call must come 
+        // after `Accounts.createUser` or `Accounts.onCreate`
+        Roles.addUsersToRoles(id, user.roles);
+      }
+      Meteor.users.update(id, {$set: {"emails.0.verified" :true}});
+    });
+  }
+
+
+  // var adminUser = Roles.getUsersInRole('admin');
 
   // if(!adminUser){
   //   adminUser = Accounts.createUser({
@@ -36,49 +63,49 @@ var adminUser = Meteor.users.findOne({roles:{$in:["admin"]}});
 // };
 
 
-Accounts.onCreateUser(function(options, user) {
-  user.profile = options.profile || {};
-  user.profile.code_verified = false;
-  
-  if(user.profile.type === 'advocate'){
-    var ph = user.profile.phone
-    var code = Math.floor(Math.random()*9000) + 1000;
-    user.profile.code = code;
+  Accounts.onCreateUser(function(options, user) {
+    user.profile = options.profile || {};
+    user.profile.code_verified = false;
+    
+    if(user.profile.type === 'advocate'){
+      var ph = user.profile.phone
+      var code = Math.floor(Math.random()*9000) + 1000;
+      user.profile.code = code;
 
-    Accounts.emailTemplates.verifyEmail.text = function(user, url) {
-      return "Dear " + user.profile.name + ",\n\n" +
-       'Here is your 4 digit verification code: ' + code +'.  Click on the link to verify code: ' + url +
-          "\n\n" + "Thanks";
-    };
-    // Session.set('phcode', code);
-    twilio = Twilio('ACc4dc855ca070a1db07f086566f561a30', 'a6ded70636ffb413789396296b12e449');
-    twilio.sendSms({
-      to: ph,
-      from: '+14028755543',
-      body: code
-    }, function(err, responseData) { 
-      if (!err) { 
-        console.log(responseData.from);
-        console.log(responseData.body);
-
-      }
-      else{
-        console.log(err)
-      }
-    });
-
-  }
-  else
-    if(user.profile.type === 'doctor'){
       Accounts.emailTemplates.verifyEmail.text = function(user, url) {
         return "Dear " + user.profile.name + ",\n\n" +
-          'To verify your account email, simply click the link below. ' + url +
-          "\n\n" + "Thanks";
+         'Here is your 4 digit verification code: ' + code +'.  Click on the link to verify code: ' + url +
+            "\n\n" + "Thanks";
       };
+      // Session.set('phcode', code);
+      twilio = Twilio('ACc4dc855ca070a1db07f086566f561a30', 'a6ded70636ffb413789396296b12e449');
+      twilio.sendSms({
+        to: ph,
+        from: '+14028755543',
+        body: code
+      }, function(err, responseData) { 
+        if (!err) { 
+          console.log(responseData.from);
+          console.log(responseData.body);
+
+        }
+        else{
+          console.log(err)
+        }
+      });
+
     }
-  
-  return user;
-});
+    else
+      if(user.profile.type === 'doctor'){
+        Accounts.emailTemplates.verifyEmail.text = function(user, url) {
+          return "Dear " + user.profile.name + ",\n\n" +
+            'To verify your account email, simply click the link below. ' + url +
+            "\n\n" + "Thanks";
+        };
+      }
+    
+    return user;
+  });
 
   Accounts.onLogin(function (info) {
     var user = info.user;
