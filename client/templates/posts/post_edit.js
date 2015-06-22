f389cb98fa213d0970df5c436861fab33bf27685
mohsin <mohsin.rafi@kwanso.com>
2015-06-22 17:43:15 +0500

    post design updated.

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,19 @@
+var postHooks = {
+  after: {
+    update: function(doc) {
+      Router.go('/show');
+    }
+  }
+}
+ 
+AutoForm.addHooks('updatePostForm', postHooks);
+
+Template.postEdit.helpers({
+    file_S3_url: function(){
+        var file = Files.findOne({_id:this.fileId});
+        if(file){
+          url = "https://s3.amazonaws.com/medcircle/upload/files/"+file._id+"-"+file.name();
+          return url;
+        }
+    },
+})
