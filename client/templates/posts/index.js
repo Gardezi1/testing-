cd3d1271b5a741b7fdf6640b0f584e1038c726e7
wal-fazil <fazil@westagilelabs.com>
2015-06-23 16:00:56 +0500

    file have been renamed and extra files is removed

---
+++ client/templates/posts/index.js
@@ -0,0 +1,17 @@
+Template.show.helpers({
+    beforeRemove: function () {
+      return function (collection, id) {
+        var doc = collection.findOne(id);
+        if (confirm('Really delete "' + doc.title + '"?')) {
+          this.remove();
+        }
+      };
+    },
+  file_S3: function(){
+        var file = Files.findOne({_id:this.fileId});
+        if(file){
+          url = "https://s3.amazonaws.com/medcircle/upload/files/"+file._id+"-"+file.name();
+          return url;
+        }
+    },
+})
--- client/templates/posts/show.js
+++ client/templates/posts/show.js
@@ -1,17 +1,12 @@
-Template.show.helpers({
-    beforeRemove: function () {
-      return function (collection, id) {
-        var doc = collection.findOne(id);
-        if (confirm('Really delete "' + doc.title + '"?')) {
-          this.remove();
-        }
-      };
-    },
+Template.postView.helpers({
+    list: function() {
+    return Posts.find({_id: this._id}); 
+  },
   file_S3: function(){
         var file = Files.findOne({_id:this.fileId});
         if(file){
           url = "https://s3.amazonaws.com/medcircle/upload/files/"+file._id+"-"+file.name();
           return url;
         }
-    },
+    },   
 })
