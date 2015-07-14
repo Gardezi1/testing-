35a15d6c9f2027e09af97546909560aeedfeb929
mohsin <mohsin.rafi@kwanso.com>
2015-07-14 16:24:01 +0500

    Invite advocates by doctor.

--- server/lib/config/seeds.js
+++ server/lib/config/seeds.js
@@ -28,6 +28,12 @@ if (Meteor.isServer) {
   }
 
   Accounts.onCreateUser(function(options, user) {
+
+    if(!options || !user) {
+      console.log('error creating user');
+      return;
+    }
+    else{
     user.profile = options.profile || {};
     user.profile.code_verified = false;
 
@@ -41,22 +47,8 @@ if (Meteor.isServer) {
          'Here is your 4 digit verification code: ' + code +'.  Click on the link to verify code: ' + url +
             "\n\n" + "Thanks";
       };
-      twilio = Twilio('ACc4dc855ca070a1db07f086566f561a30', 'a6ded70636ffb413789396296b12e449');
-      twilio.sendSms({
-        to: ph,
-        from: '+14028755543',
-        body: "Dear " + user.profile.name + ",\n\n" +'Your 4 pin code is: '+code
-      }, function(err, responseData) {
-        if (!err) {
-          console.log(responseData.from);
-          console.log(responseData.body);
 
-        }
-        else{
-          console.log(err)
-        }
-      });
-
+      Meteor.call('sendTwilioMessage',user, ph, code);
     }
     else
       if(user.profile.type === 'doctor'){
@@ -68,6 +60,7 @@ if (Meteor.isServer) {
       }
 
     return user;
+    }
   });
 
   Accounts.onLogin(function (info) {
@@ -87,8 +80,8 @@ if (Meteor.isServer) {
   Meteor.startup(function(){
     if(Topics.find().count() == 0){
       ["Topic1", "Topic2", "Topic3", "Topic4", "Topic5", "Topic6", "Topic7", "Topic8", "Topic9", "Topic10", "Topic11", "Topic12"].forEach(function(topic){
-      Topics.insert({name:topic})
-    });
+        Topics.insert({name:topic})
+      });
     }
   });
 
