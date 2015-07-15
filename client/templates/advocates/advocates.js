f740b8e8b7346d05eae5a058e4514f284cec148f
mohsin <mohsin.rafi@kwanso.com>
2015-07-15 18:30:07 +0500

    Advocate circle listing updated.

--- client/templates/advocates/advocates.js
+++ client/templates/advocates/advocates.js
@@ -13,5 +13,42 @@ Template.advocatesListing.helpers({
       url = "https://s3.amazonaws.com/medcircle/upload/data/"+file._id+"-"+file.name();
         return url;
     }
+  },
+  isChecked: function(uid){
+    var id = this._id;
+    // console.log(id);
+    circle = Meteor.users.findOne({_id:uid}).profile.doctorCircle;
+    if(circle == undefined || circle.length <= 0){
+      return "";
+    }
+    if(circle.indexOf("1st") >= 0){
+      return "checked";
+    }
+    else{
+      return "";
+    }
   }
+});
+
+Template.advocatesListing.events({  
+  'click input': function(e) {
+    console.log("inn");
+    var id = $(e.target).attr('id');
+    console.log(id);
+    circle = Meteor.users.findOne({_id:id}).profile.doctorCircle;
+    if(circle == undefined || circle.length <= 0){
+      // console.log("push")
+      Meteor.users.update(id, {$set: { "profile.doctorCircle": "1st"}});
+    }
+    else
+    if(circle.indexOf("1st") >= 0){
+      // console.log("pop");
+      Meteor.users.update(id, {$set: { "profile.doctorCircle": "2nd"}});
+    }
+    else
+    if(circle.indexOf("2nd") >= 0){
+      // console.log("pop");
+      Meteor.users.update(id, {$set: { "profile.doctorCircle": "1st"}});
+    }
+   }
 });
