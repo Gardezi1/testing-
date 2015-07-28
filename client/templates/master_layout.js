851ff1b7a3f6e99859761dab976c966b840a2f24
mohsin <mohsin.rafi@kwanso.com>
2015-07-28 22:34:12 +0500

    user profile following added.

--- client/templates/master_layout.js
+++ client/templates/master_layout.js
@@ -1,5 +1,29 @@
 Template.masterLayout.events({  
   'click .save-my-profile': function(event) {
     $(".save-profile").click();
-  }
+  },
+  'click .start-follow': function(e){
+    var id = $(e.target).attr('id');
+    console.log(id);
+    e.preventDefault();
+    Meteor.call('addToFollowing', id, function(id,error, result){
+      if(error){
+        console.log("error from addToFollowing: ", error);
+      } else {
+        // console.log(id);
+      } 
+    });
+  },
+  // 'click .start-unfollow': function(e){
+  //   var id = $(e.target).attr('id');
+  //   console.log(id);
+  //   e.preventDefault();
+  //   Meteor.call('removeFromFollowing', id, function(id,error, result){
+  //     if(error){
+  //       console.log("error from removeFromFollowing: ", error);
+  //     } else {
+  //       // console.log(id);
+  //     } 
+  //   });
+  // }
 });
