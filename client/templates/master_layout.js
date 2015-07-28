3cb491aa458fe2b53738f8ce30f123e5322edf71
mohsin <mohsin.rafi@kwanso.com>
2015-07-28 22:57:00 +0500

    user profile following updated.

--- client/templates/master_layout.js
+++ client/templates/master_layout.js
@@ -3,7 +3,7 @@ Template.masterLayout.events({
     $(".save-profile").click();
   },
   'click .start-follow': function(e){
-    var id = $(e.target).attr('id');
+    var id = $(".start-follow").attr('id');
     console.log(id);
     e.preventDefault();
     Meteor.call('addToFollowing', id, function(id,error, result){
@@ -14,16 +14,16 @@ Template.masterLayout.events({
       } 
     });
   },
-  // 'click .start-unfollow': function(e){
-  //   var id = $(e.target).attr('id');
-  //   console.log(id);
-  //   e.preventDefault();
-  //   Meteor.call('removeFromFollowing', id, function(id,error, result){
-  //     if(error){
-  //       console.log("error from removeFromFollowing: ", error);
-  //     } else {
-  //       // console.log(id);
-  //     } 
-  //   });
-  // }
+  'click .start-unfollow': function(e){
+    var id = $(".start-unfollow").attr('id');
+    console.log(id);
+    e.preventDefault();
+    Meteor.call('removeFromFollowing', id, function(id,error, result){
+      if(error){
+        console.log("error from removeFromFollowing: ", error);
+      } else {
+        // console.log(id);
+      } 
+    });
+  }
 });
