4a24892cf0c8b22abdc6a5a4825e31a029dfb412
mohsin <mohsin.rafi@kwanso.com>
2015-09-03 15:34:31 +0500

    login error handling updated.

--- client/templates/application/home.js
+++ client/templates/application/home.js
@@ -8,8 +8,19 @@ Template.home.events({
     var emailVar = $("#advocateEmail").val();
     var passwordVar = $("#advocatePassword").val();
     user = Meteor.users.findOne({"emails.address": emailVar});
-    if(Roles.userIsInRole(user._id, [ROLES.Doctor])){
-      sAlert.error("Please use your appropriate tab to login.", {effect: 'genie', position: 'top-right', timeout: 5000, onRouteClose: false, stack: false, offset: '80px'});
+    if(user){
+      if(Roles.userIsInRole(user._id, [ROLES.Doctor])){
+        sAlert.error("Please use your appropriate tab to login.", {effect: 'genie', position: 'top-right', timeout: 5000, onRouteClose: false, stack: false, offset: '80px'});
+      }
+      else{
+        Meteor.loginWithPassword(emailVar, passwordVar, function(err) {
+          if (err) {
+            sAlert.error(err.reason, {effect: 'genie', position: 'top-right', timeout: 5000, onRouteClose: false, stack: false, offset: '80px'});
+          } else {
+            Router.go('/articles');
+          }
+        });
+      }
     }
     else{
       Meteor.loginWithPassword(emailVar, passwordVar, function(err) {
@@ -26,8 +37,19 @@ Template.home.events({
     var emailVar = $("#doctorEmail").val();
     var passwordVar = $("#doctorPassword").val();
     user = Meteor.users.findOne({"emails.address": emailVar});
-    if(Roles.userIsInRole(user._id, [ROLES.Advocate])){
-      sAlert.error("Please use your appropriate tab to login.", {effect: 'genie', position: 'top-right', timeout: 5000, onRouteClose: false, stack: false, offset: '80px'});
+    if(user){
+      if(Roles.userIsInRole(user._id, [ROLES.Advocate])){
+        sAlert.error("Please use your appropriate tab to login.", {effect: 'genie', position: 'top-right', timeout: 5000, onRouteClose: false, stack: false, offset: '80px'});
+      }
+      else{
+        Meteor.loginWithPassword(emailVar, passwordVar, function(err) {
+          if (err) {
+            sAlert.error(err.reason, {effect: 'genie', position: 'top-right', timeout: 5000, onRouteClose: false, stack: false, offset: '80px'});
+          } else {
+            Router.go('/articles');
+          }
+        });
+      }
     }
     else{
       Meteor.loginWithPassword(emailVar, passwordVar, function(err) {
