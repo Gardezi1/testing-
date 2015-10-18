9c146994a5d4a3aa65336d689a7f80296697d2d8
Mohsin Rafi <mohsin@westagilelabs.com>
2015-10-19 00:59:19 +0500

    cameratag integrated.

--- web/client/templates/posts/index.js
+++ web/client/templates/posts/index.js
@@ -120,36 +120,4 @@ Template.articleList.events({
   'click .pluse-icon': function(event) {
     $('.pluse-icon').tooltip( "close");
   }
-  // 'click .record-video': function () {
-  //   if (Meteor.isCordova){
-  //     navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:1, duration: 13});
-  //   } else {
-  //     // do the standard mdg:camera thing here ??
-  //     // because we're in a browser.....
-  //   }
-  // }
 });
-
-
-// if (Meteor.isClient) {
-
-//   if (Meteor.isCordova) {
-//     Meteor.startup(function(){
-//       console.log("device capability: " + JSON.stringify(navigator.device.capture));
-//   });
-// }
-
-//   var captureError = function(error) {
-//     navigator.notification.alert('ERROR:' + error.message, null, "Capture Error");
-//   }
-
-//   var captureSuccess = function(mediaFiles) {
-//     var i, path, len;
-//     for (i=0, len = mediaFiles.length; i < len; i += 1) {
-//       path = mediaFiles[i].fullPath;
-
-//       // do something with this file... upload to S3 ?
-//       console.log("path = " + path);
-//     }
-//   }
-// }
--- web/client/templates/profile/index.js
+++ web/client/templates/profile/index.js
@@ -11,5 +11,58 @@ Template.profileView.helpers({
   },
   ifDoctor: function(){ 
    return Meteor.user().profile.type == "doctor";
+  },
+  onWeb: function(){
+    if(Meteor.isCordova)
+      return false;
+    else
+      return true;
   }
 });
+
+Template.profileView.events({
+  'click .record-video': function () {
+    if (Meteor.isCordova){
+      navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:1, duration: 13});
+    } else {
+      // do something for web.
+    }
+  }
+})
+
+
+if (Meteor.isClient) {
+
+  if (Meteor.isCordova) {
+      Meteor.startup(function(){
+        console.log("device capability: " + JSON.stringify(navigator.device.capture));
+    });
+  }
+  else{
+    // Meteor.setTimeout(function(){
+    //   console.log("inside");
+    //   CameraTag.observe('Medcircle', 'initialized', function(){
+    //     console.log("inside camera");
+    //     var myCamera = CameraTag.cameras["Medcircle"];
+    //     var myVideo = myCamera.getVideo();
+    //     console.log("vid"+ myVideo);
+    //     var mp4_url = myVideo.formats[0].mp4_url;
+    //     console.log("url"+mp4_url);
+    //   });
+    // }, 1000);
+  }
+
+  var captureError = function(error) {
+    navigator.notification.alert('ERROR:' + error.message, null, "Capture Error");
+  }
+
+  var captureSuccess = function(mediaFiles) {
+    var i, path, len;
+    for (i=0, len = mediaFiles.length; i < len; i += 1) {
+      path = mediaFiles[i].fullPath;
+
+      // do something with this file... upload to S3 ?
+      console.log("path = " + path);
+    }
+  }
+}
